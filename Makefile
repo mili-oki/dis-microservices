9057c7c4d01da34536c1a6ce388bc4a953e826b4
mili-oki <103196521+mili-oki@users.noreply.github.com>
2025-09-25 16:36:14 +0200

    fix build failure

---
+++ Makefile
@@ -0,0 +1,12 @@
+build:
+\tdocker compose build
+up:
+\tdocker compose up -d
+down:
+\tdocker compose down
+logs:
+\tdocker compose logs -f
+ps:
+\tdocker compose ps
+purge:
+\tdocker compose down -v --remove-orphans
--- gateway/src/test/java/com/example/dis/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/dis/gateway/GatewayApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.dis.gateway;
-
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class GatewayApplicationTests {
 
 	@Test

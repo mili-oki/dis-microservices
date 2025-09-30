8a58c20bd07e3348a42cba946d8de3922fc885ff
mili-oki <103196521+mili-oki@users.noreply.github.com>
2025-10-01 00:17:08 +0200

    fix circural issue

--- orders-service/src/main/java/com/example/dis/orders_service/client/CatalogClient.java
+++ orders-service/src/main/java/com/example/dis/orders_service/client/CatalogClient.java
@@ -2,7 +2,6 @@ package com.example.dis.orders_service.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "catalog-service")
 @CircuitBreaker(name = "catalog-service")
 @Retry(name = "catalog-service")
-@TimeLimiter(name = "catalog-service")
 public interface CatalogClient {
     @GetMapping("/products/{id}")
     ProductDto getProductById(@PathVariable("id") Long id);

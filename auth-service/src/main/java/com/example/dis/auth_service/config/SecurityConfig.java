df872b5bdcf5b9157d1ac01b9c03437de64b68e8
mili-oki <103196521+mili-oki@users.noreply.github.com>
2025-09-30 14:27:51 +0200

    Move SecurityConfig to config package

---
+++ auth-service/src/main/java/com/example/dis/auth_service/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.dis.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+      .csrf(csrf -> csrf.disable())
+      .httpBasic(b -> b.disable())
+      .formLogin(f -> f.disable())
+      .authorizeHttpRequests(a -> a
+         .requestMatchers("/actuator/**").permitAll()     
+        .requestMatchers("/auth/**").permitAll()  
+        .anyRequest().authenticated()
+      )
+      .build();
+  }
+}
